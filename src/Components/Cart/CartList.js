a796e69ce98d7703ff2e6f96ba9757c374abe741
shakoorbhatti <o3335616177@gmail.com>
2019-11-20 20:25:54 +0500

    PayPall Button

--- src/Components/Cart/CartList.js
+++ src/Components/Cart/CartList.js
@@ -8,9 +8,8 @@ export default function CartList({value}) {
         <div className="container-fluid">
             {cart.map(item=>{
                 return <CartItem key={item.id} item={item} value={value}/>
-            })}
-            <h1>Hello From CartList</h1>
-            
+            }
+            )}
         </div>
     )
 }
